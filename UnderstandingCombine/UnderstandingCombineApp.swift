d593872078372322f6ed87767d44cdc891062c3e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-19 20:44:02 +0300

    Initial Commit

---
+++ UnderstandingCombine/UnderstandingCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingCombineApp.swift
+//  UnderstandingCombine
+//
+//  Created by ramil on 19.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
